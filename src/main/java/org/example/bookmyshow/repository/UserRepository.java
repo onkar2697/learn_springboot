025a9d2cc9fa00a5b14f9f4c2b6c789978e92b01
onkar <onkarnaske26@gmail.com>
2026-08-12 18:57:49 +0530

    bookmyshow application

--- src/main/java/org/example/bookmyshow/repository/UserRepository.java
+++ src/main/java/org/example/bookmyshow/repository/UserRepository.java
@@ -4,5 +4,6 @@ import org.example.bookmyshow.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    public User FindByEmail(String email);
 
 }
