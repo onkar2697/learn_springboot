02f63d846b6e9eec312be0936cd898ed921f5e09
onkar <onkarnaske26@gmail.com>
2026-08-14 19:41:54 +0530

    bookmyshow application

--- src/main/java/org/example/bookmyshow/repository/UserRepository.java
+++ src/main/java/org/example/bookmyshow/repository/UserRepository.java
@@ -2,14 +2,17 @@ package org.example.bookmyshow.repository;
 
 import org.example.bookmyshow.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
-     Optional<User> findByEmail(String email);
+    @Query("SELECT u FROM User u WHERE email=?1")
+    Optional<User> findByEmail(String email);
 
+    @Query("SELECT u FROM User u WHERE name=?1")
     List<User> findByName(String name);
 
 
