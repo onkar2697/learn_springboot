b49174788214090fe88caeecabe0e30e3f3e9c1f
onkar <onkarnaske26@gmail.com>
2026-08-27 19:28:55 +0530

    bookmyshow application

--- src/main/java/org/example/bookmyshow/entity/User.java
+++ src/main/java/org/example/bookmyshow/entity/User.java
@@ -12,9 +12,11 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     //Let mysql/database generate the ids it generates id sequestially
-    private long  id;
+    private long  Id;
     private String name;
     private String email;
+    private long age;
+    private String password;
 
     @OneToMany(
             mappedBy = "user",cascade = CascadeType.ALL
@@ -28,10 +30,10 @@ public class User {
 
 
     public long getId() {
-        return id;
+        return Id;
     }
     public void setId(long id) {
-        this.id = id;
+        this.Id = id;
     }
 
     public String getName() {
@@ -53,5 +55,21 @@ public class User {
     }
     public void setBookings(List<Booking> bookings) {
         this.bookings = bookings;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getAge() {
+        return age;
+    }
+
+    public void setAge(long age) {
+        this.age = age;
     }
 }
