5056e7c0c4bfe9d8e7e8b9767811079c16880c47
onkar <onkarnaske26@gmail.com>
2026-08-28 22:57:45 +0530

    bookmyshow application

--- src/main/java/org/example/bookmyshow/repository/BookingRepository.java
+++ src/main/java/org/example/bookmyshow/repository/BookingRepository.java
@@ -1,11 +1,7 @@
 package org.example.bookmyshow.repository;
 
 import org.example.bookmyshow.entity.Booking;
-import org.example.bookmyshow.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking,Long> {
 
