1fcbff66211e1d88536bf0e37a26e7b013940c67
onkar <onkarnaske26@gmail.com>
2026-09-09 22:26:19 +0530

    bookmyshow application

--- src/main/java/org/example/bookmyshow/specifications/UserSpecifications.java
+++ src/main/java/org/example/bookmyshow/specifications/UserSpecifications.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.domain.Specification;
 public class UserSpecifications {
     public static Specification<User> hasName(String name){       //using specification JPA
         return ((root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("name"),name));
+                criteriaBuilder.equal(root.get("name"),name));                     // using CriteriaBuilder
     }
 
     public static Specification<User> hasAgeGreaterThanOrEqualTo(Integer age){
