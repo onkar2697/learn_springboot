04b4b600d7c74d4c0296a0b326222c9a7e21f1ba
onkar <onkarnaske26@gmail.com>
2026-09-18 23:08:05 +0530

    bookmyshow application

--- src/main/java/org/example/bookmyshow/entity/User.java
+++ src/main/java/org/example/bookmyshow/entity/User.java
@@ -3,11 +3,16 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
     @Id
@@ -23,6 +28,11 @@ public class User {
     @NotBlank
     @Size(min = 6,max =15, message = "Password should be greater than 6 and less than 15 characters")
     private String password;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 
     @OneToMany(
             mappedBy = "user",cascade = CascadeType.ALL
