824933b8bfdd4a7a17acee830e62db3ca1a1f901
onkar <onkarnaske26@gmail.com>
2026-09-18 23:43:43 +0530

    bookmyshow application

---
+++ src/main/java/org/example/bookmyshow/config/AuditorAwareImpl.java
@@ -0,0 +1,16 @@
+package org.example.bookmyshow.config;
+
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("Admin");
+    }
+}
