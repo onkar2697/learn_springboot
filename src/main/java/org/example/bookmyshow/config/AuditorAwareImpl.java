19136828635199215f8ca3ee6601d5f6ba6c5692
onkar <onkarnaske26@gmail.com>
2026-09-21 22:53:28 +0530

    bookmyshow application

--- src/main/java/org/example/bookmyshow/config/AuditorAwareImpl.java
+++ src/main/java/org/example/bookmyshow/config/AuditorAwareImpl.java
@@ -4,6 +4,7 @@ package org.example.bookmyshow.config;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
 
+
 import java.util.Optional;
 
 @Component
@@ -11,6 +12,15 @@ public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
     public Optional<String> getCurrentAuditor() {
+//        Authentication authentication =
+//                SecurityContextHolder.getContext().getAuthentication();
+//
+//        if (authentication == null ||
+//                !authentication.isAuthenticated()) {
+//            return Optional.of("SYSTEM");
+//        }
+//
+//        return Optional.of(authentication.getName());
         return Optional.of("Admin");
-    }
+    }    // we define who does the modificsation or Updated by
 }
